22899aec6902c9c4179b298bb5620abfd4ee4337
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-27 18:08:12 +0530

    DATABASE CONENECTED

--- register.php
+++ register.php
@@ -1,15 +1,31 @@
 <?php
 $servername = "localhost";
 $username = "id12328517_rtcs1234";
-$password = "Archiit@123";                                                                                                                                                      ;
+$password = "Archit@123";                                                                                                                                                      
 $dbname = "id12328517_rtcs1234";
 
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email=$_POST['email'];
+$age=$_POST['age'];
+$gender=$_POST['gender'];
+$mobile=$_POST['mobile'];
+$passwordd=$_POST['passwordd'];
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+else
+{$sql = ("INSERT INTO USERS (firstname, lastname, email, age, mobile, gender, passwordd) VALUES ('$firstname','$lastname','$email','$age','$gender','$mobile','$passwordd')");
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+}
 ?>
