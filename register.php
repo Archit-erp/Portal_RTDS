7916824f454bb714d713e0e2e2f352894f7a06d7
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 11:12:50 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,33 +1,28 @@
 <?php
+class DBController{
+private $servername = "35.238.89.60";
+private $username = "test_userr";
+private $passwordd = "Password@123";        
+private $dbname = "user";
+    
+private static $conn;
+    
+public $firstname=$_POST['firstname'];
+public $lastname=$_POST['Lastname'];
+public $email=$_POST['Email'];
+public $age=$_POST['age'];
+public $gender=$_POST['gender'];
+public $mobile=$_POST['MobileNo'];
+public $password=$_POST['ConfirmPassword'];
 
-$servername = "35.238.89.60";
-$username = "test_userr";
-$passwordd = "Password@123";        
-$dbname = "user";
-$firstname=$_POST['firstname'];
-$lastname=$_POST['Lastname'];
-$email=$_POST['Email'];
-$age=$_POST['age'];
-$gender=$_POST['gender'];
-$mobile=$_POST['MobileNo'];
-$password=$_POST['ConfirmPassword'];
-$flag=$_POST['flag'];
+function __construct(){
+    $this->conn = $this->connectDB();
+}
 
-
+function connectDB(){
 if(empty($email || $password))
 {
     header("Location: http://35.238.89.60/Portal_RTDS/index.php");
-}
-else if(!empty($flag))
-{
-    console.log('enter1');
-    $conn = new mysqli($servername, $username, $passwordd, $dbname);
-         $result = $conn->query("SELECT * FROM user1 WHERE email='".$email."'");
-           $rowcount = $result->num_rows;
-                if($rowcount>0){
-                   echo "<span class='status-not-available'>Email Already Exist!</span>" ;}
-                else{
-                    echo "<span class='status-available'>Email Available</span>";}
 }
 
 else{
@@ -35,17 +30,22 @@ else{
     $conn = new mysqli($servername, $username, $passwordd, $dbname);
         // Check connection
         if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-                                    }
+                die("Connection failed: " . $conn->connect_error);}
         else
-        {
-         $sql = ("INSERT INTO user1 (firstname, lastname, email, age, mobile, gender, password) VALUES ('$firstname','$lastname','$email','$age','$mobile','$gender','$password')");
-            if (mysqli_query($conn, $sql)) {
+        { $this->insertDB();}
+    }
+ function insertDB(){
+ 
+ $query = ("INSERT INTO user1 (firstname, lastname, email, age, mobile, gender, password) VALUES ('$firstname','$lastname','$email','$age','$mobile','$gender','$password')");
+            if (mysqli_query($this->conn, $query)) {
                     echo "New record created successfully";
             }
             else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
-}}
+ 
+ } 
+    
 $mysqli->close();
+}
 ?>
