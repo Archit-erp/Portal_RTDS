c41ca9a42c4ffef04957aea95b01c8ef0165c081
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 13:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 									<div class="col-sm-12" id="space"><input class="form-control" type="email" onkeyup="checkStatus()" id="email"  name="Email" placeholder="Email" required></div>
 								</div>
 								<div class="row">
-								</div class="col-sm-12" id="space"><div class="col-sm-2"><span id="statusCheck"></span></div>
+								</div class="col-sm-12" id="space"><span id="statusCheck"></span></div>
 								</div>
 								<div class="row">
 									<div class="col-sm-12"><label>Gender</label></div>
