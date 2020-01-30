18e1b8e678c277757215fbbe86d89129122854aa
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 12:56:58 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,6 +4,9 @@ $servername = "35.238.89.60";
 $username = "test_userr";
 $passwordd = "Password@123";        
 $dbname = "user";
+
+
+
 $firstname=$_POST['firstname'];
 $lastname=$_POST['Lastname'];
 $email=$_POST['Email'];
@@ -11,24 +14,13 @@ $age=$_POST['age'];
 $gender=$_POST['gender'];
 $mobile=$_POST['MobileNo'];
 $password=$_POST['ConfirmPassword'];
-$flag=$_POST['flag'];
+
 
 
 if(empty($email || $password))
 {
     header("Location: http://35.238.89.60/Portal_RTDS/index.php");
 }
-else if(!empty($flag))
-{
-    $conn = new mysqli($servername, $username, $passwordd, $dbname);
-         $result = $conn->query("SELECT * FROM user1 WHERE email='".$email."'");
-           $rowcount = $result->num_rows;
-                if($rowcount>0){
-                   echo "<span class='status-not-available'>Email Already Exist!</span>" ;}
-                else{
-                    echo "<span class='status-available'>Email Available</span>";}
-}
-
 else{
 // Create connection
     $conn = new mysqli($servername, $username, $passwordd, $dbname);
@@ -45,6 +37,6 @@ else{
             else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
-}}
+}
 $mysqli->close();
 ?>
