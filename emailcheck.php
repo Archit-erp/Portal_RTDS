34ba9c9afc9a19d5ead8b995468a71e40e8924c6
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 13:12:10 +0530

    Create emailcheck.php

---
+++ emailcheck.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "35.238.89.60";
+$username = "test_userr";
+$passwordd = "Password@123";        
+$dbname = "user";
+
+$conn = mysqli_connect($servername, $username, $passwordd, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                                    }
+else
+{
+
+(isset($_POST['email'])){
+  $email=$_POST['email'];
+  
+  $query= "select count(*) as CntUser from users where email='".$email."'";
+  $result = mysqli_query($conn,$query);
+  $response="<span style='color :green'>Available.</span>";
+    if(mysqli_num_rows($result)){
+    
+    $row=mysqli_fetch_array($result);
+    $count=$row['CntUser'];
+      if($count>0){
+      $response = "<span style='color :red;'>Not Available.</span>";
+      }
+    
+    
+    }
+  echo $response;
+  die;
+}
+
+
+?>
