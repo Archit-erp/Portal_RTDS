8632a90ac556d172cda40aaa2a410c4962e4689d
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 16:37:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 									<div class="col-sm-12" id="space"><input class="form-control" type="email" onkeyup="checkStatus();" id="email"  name="Email" placeholder="Email" autocomplete="off" required></div>
 								</div>
 								
-								<span><img src = "2.gif" id = "loader" style = "display:none"></p><div id="statusCheck"></div>
+								<span><img src = "2.gif" id = "loader" style = "display:none"><div id="statusCheck"></div>
 								
 						
 								<div class="row">
@@ -85,7 +85,7 @@
 					method : "POST",
 					success:function(response){
 					$("#statusCheck").html(response);
-					$("#loader").hide();
+					
 				}});
 				
 				}
