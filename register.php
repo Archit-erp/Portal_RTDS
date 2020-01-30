da6b9185ca9f4b04435afd1f2c38a1976ef2ab95
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 11:41:37 +0530

    Update register.php

--- register.php
+++ register.php
@@ -7,44 +7,29 @@ private $dbname = "user";
     
 private static $conn;
     
-public $firstname=$_POST['firstname'];
-public $lastname=$_POST['Lastname'];
-public $email=$_POST['Email'];
-public $age=$_POST['age'];
-public $gender=$_POST['gender'];
-public $mobile=$_POST['MobileNo'];
-public $password=$_POST['ConfirmPassword'];
-
 function __construct(){
-    $this->conn = $this->connectDB();
+     $conn = new mysqli($servername, $username, $passwordd, $dbname);
+            if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);)
 }
 
-function connectDB(){
+function insertDB($query){
 if(empty($email || $password))
 {
     header("Location: http://35.238.89.60/Portal_RTDS/index.php");
 }
 
 else{
-// Create connection
-    $conn = new mysqli($servername, $username, $passwordd, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);}
-        else
-        { $this->insertDB();}
-    }
- function insertDB(){
- 
- $query = ("INSERT INTO user1 (firstname, lastname, email, age, mobile, gender, password) VALUES ('$firstname','$lastname','$email','$age','$mobile','$gender','$password')");
-            if (mysqli_query($this->conn, $query)) {
-                    echo "New record created successfully";
-            }
+    if (mysqli_query($this->conn, $query)) {
+                    $value=True;
+                return $value;
+        }
             else {
+                $value=False;
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                return $value;
             }
- 
- } 
+}}
     
 $mysqli->close();
 }
