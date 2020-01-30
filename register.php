ab2e38c88f8c6e68124409e86e67ff8945f9bf84
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 11:57:32 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,37 +1,50 @@
 <?php
-class DBController{
-private $servername = "35.238.89.60";
-private $username = "test_userr";
-private $passwordd = "Password@123";        
-private $dbname = "user";
-    
-private static $conn;
-    
-function __construct(){
-    console.log('hello');
-     $conn = new mysqli($servername, $username, $passwordd, $dbname);
-            if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);)
-}
 
-function insertDB($query){
+$servername = "35.238.89.60";
+$username = "test_userr";
+$passwordd = "Password@123";        
+$dbname = "user";
+$firstname=$_POST['firstname'];
+$lastname=$_POST['Lastname'];
+$email=$_POST['Email'];
+$age=$_POST['age'];
+$gender=$_POST['gender'];
+$mobile=$_POST['MobileNo'];
+$password=$_POST['ConfirmPassword'];
+$flag=$_POST['flag'];
+
+
 if(empty($email || $password))
 {
     header("Location: http://35.238.89.60/Portal_RTDS/index.php");
 }
+else if(!empty($flag))
+{
+    $conn = new mysqli($servername, $username, $passwordd, $dbname);
+         $result = $conn->query("SELECT * FROM user1 WHERE email='".$email."'");
+           $rowcount = $result->num_rows;
+                if($rowcount>0){
+                   echo "<span class='status-not-available'>Email Already Exist!</span>" ;}
+                else{
+                    echo "<span class='status-available'>Email Available</span>";}
+}
 
 else{
-    if (mysqli_query($this->conn, $query)) {
-                    $value=True;
-                return $value;
-        }
+// Create connection
+    $conn = new mysqli($servername, $username, $passwordd, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                                    }
+        else
+        {
+         $sql = ("INSERT INTO user1 (firstname, lastname, email, age, mobile, gender, password) VALUES ('$firstname','$lastname','$email','$age','$mobile','$gender','$password')");
+            if (mysqli_query($conn, $sql)) {
+                    echo "New record created successfully";
+            }
             else {
-                $value=False;
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                return $value;
             }
 }}
-    
 $mysqli->close();
-}
 ?>
