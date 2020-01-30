8f66b2439312b5db9234f802833a2622a5964783
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-30 10:41:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
 				jQuery.ajax({
 					url: "register.php",
 					data: 'email='+$("#email").val(),
-					data2: 'flag='+$("flag").val(1),
+					//data2: 'flag='+$("flag").val(1),
 					type : "POST",
 					success:function(data){
 					$("#statusCheck").html(data);
