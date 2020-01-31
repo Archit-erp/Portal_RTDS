8d475d48c1c16c1fd121c59c73888008523da15c
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-31 12:22:05 +0530

    Update emailcheck.php

--- emailcheck.php
+++ emailcheck.php
@@ -1,38 +1,38 @@
 <?php
 $servername = "35.238.89.60";
 $username = "test_userr";
-$passwordd = "Password@123";        
+$passwordd = "Password@123";
 $dbname = "user";
 
 
 $conn =  new mysqli($servername, $username, $passwordd, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+else
+{
+    if(!empty($_POST['email']))
+    {
+        $email=$_POST['email'];
+
+        $query= "select * FROM user1 where email='".$email."'";
+        $result = mysqli_query($conn,$query);
+        $rowcount = mysqli_num_rows($result);
+
+
+        if($rowcount>0){
+            $response = 1;
         }
-      
-       else
-        {       
-                if(!empty($_POST['email']))
-                {
-                                $email=$_POST['email'];
+        else{
+            $response = 0;
 
-                                  $query= "select * FROM user1 where email='".$email."'";
-                                    $result = mysqli_query($conn,$query);
-                                      $rowcount = mysqli_num_rows($result);
-                                                
-                                        
-                                        if($rowcount>0){
-                                          $response = "<span style = 'color :red'>Email Already Registered.</span>";
-                                                }
-                                        else{
-                                         $response = "<span style = 'color :green'>Available.</span>";
-                                                
-                                        }
-              
-                }
+        }
 
-                echo $response;
-            }
+    }
 
- ?>
+    echo $response;
+}
+
+?>
