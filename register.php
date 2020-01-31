8eec3c9f03af72b73f45c06bf2fbae7402c4d796
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-01-31 12:22:14 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: archit.singhal
+ * Date: 31-01-2020
+ * Time: 12:13
+ */<?php
 
 $servername = "35.238.89.60";
 $username = "test_userr";
-$passwordd = "Password@123";        
+$passwordd = "Password@123";
 $dbname = "user";
 
 
@@ -24,30 +30,30 @@ if(empty($email || $password))
 else{
 // Create connection
     $conn = new mysqli($servername, $username, $passwordd, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-                                    }
-        else
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else
+    {
+        $query= "select * FROM user1 where email='".$email."'";
+        $result = mysqli_query($conn,$query);
+        $rowcount = mysqli_num_rows($result);
+
+        if($rowcount>0)
         {
-            $query= "select * FROM user1 where email='".$email."'";
-                $result = mysqli_query($conn,$query);
-                      $rowcount = mysqli_num_rows($result); 
-                
-                    if($rowcount>0)
-                    {
-                        echo "Email Already registered";
-                    }
-            
-            
-        else{ 
+            echo "Email Already registered";
+        }
+
+
+        else{
             $sql = ("INSERT INTO user1 (firstname, lastname, email, age, mobile, gender, password) VALUES ('$firstname','$lastname','$email','$age','$mobile','$gender','$password')");
             if (mysqli_query($conn, $sql)) {
-                    echo "New record created successfully";
+                echo "New record created successfully";
             }
             else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }}
-}}
+    }}
 
 ?>
