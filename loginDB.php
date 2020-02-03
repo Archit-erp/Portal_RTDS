260857ab0141a2d7e29c3a6a769a3a9cd84f54f5
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-03 12:22:47 +0530

    Update loginDB.php

--- loginDB.php
+++ loginDB.php
@@ -4,9 +4,10 @@ $username = "test_userr";
 $passwordd = "Password@123";
 $dbname = "user";
 
+session_start();
+$email = $_POST['Email'];
+$password = $_POST['Password'];
 
-$email=$_POST['Email'];
-$password=$_POST['Password'];
 
 $conn = new mysqli($servername, $username, $passwordd, $dbname);
 // Check connection
@@ -14,17 +15,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else{
-
+        $_SESSION['user_id']= $email;
     $query = "SELECT email, password FROM user1 WHERE username = '".$email."' AND password = '".$password."'";
     $result = mysqli_query($conn,$query);
 
     if(mysqli_num_rows($result)>0)
     {
-        echo 'Succesfull, Login';
+
+        header("Location : http://35.238.89.60/Portal_RTDS/Dashboard.php");
     }
 
     else{
-        echo 'Not a succesfull login';
+        header("Location : http://35.238.89.60/Portal_RTDS/login.php");
     }
 }
 
