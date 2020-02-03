cf4f10ca43d1d1b0a7c75a047aaadfdda77124cd
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-03 17:28:58 +0530

    Update loginDB.php

--- loginDB.php
+++ loginDB.php
@@ -1,38 +1,100 @@
-<?php
-$servername = "35.238.89.60";
-$username = "test_userr";
-$passwordd = "Password@123";
-$dbname = "user";
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-session_start();
-$email = $_POST['email'];
-$password = $_POST['password'];
+<div class = "container">
+    <br>
+    <h1 class="text-center" id="h1"> LOGIN!</h1>
+    <br>
 
-$conn = new mysqli($servername, $username, $passwordd, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else{
+    <form method = "post" onsubmit=" return validate();" action = "Dashboard.php">
 
-    $query = "SELECT email, password FROM user1 WHERE email = '".$email."' AND password = '".$password."'";
-    $result = mysqli_query($conn,$query);
-    $rowcount = mysqli_num_rows($result);
+        <div class="row">
+            <div class="col-sm-12"><label>Email</label></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4" id="space"><input class="form-control" type="email" id="email"  name="Email" placeholder="Email"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4"><label>Password</label></div>
+        </div>
 
-    if($rowcount>0)
-    {
-        $_SESSION['user_id']= $email;
-        $_SESSION['pass'] = $password;
+        <div class="row">
+            <div class="col-sm-4"><input class="form-control" type="password" id="pass" name="Password" placeholder="Password"><br></div>
+        </div>
 
-        $response = 1;
+        <br>
+        <center>
+            <input type="submit" id="sub"  class="btn btn-default" value="Submit">
+        </center>
+        <h5>Not A Member?<a href="index.php"> Click to register</a></h5>
+    </form>
+</div>
+
+
+
+</body>
+<script>
+
+    function validate(){
+
+        var filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        var em = document.getElementById('email').value;
+        var p = document.getElementById('pass').value;
+
+
+        /*  if(!em.match(filter)){
+
+              alert("Please Enter Valid Email Address")
+              email.focus();
+              return false;
+          }*/
+
+        if( em == "" || p == "")
+        {
+            alert("Pleade Fill the Details completely!");
+            pass.focus();
+            return false;
+        }
+
+        else{
+            var response;
+            var email = $('#email').val();
+            var pass = $('#pass').val();
+
+            $.ajax({
+                url : "loginDB.php",
+                data : {email : email , password : pass},
+                method : "POST",
+                success:function(response){
+                    if(response == 1)
+                    {
+                        return true;
+                    }
+
+                    else if(response == 0)
+                    {
+                        alert('Wrong credentials');
+                        return false;
+
+                    }
+
+                }
+            })
+
+        }
+
     }
 
-    else{
 
-        $response = 0;
-    }
 
-    echo $response;
-}
-
-?>
+</script>
+</html>
