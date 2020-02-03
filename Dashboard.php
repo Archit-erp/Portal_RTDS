402a0c9a76055e0f18491fa2bc01451de5290e41
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-03 17:28:07 +0530

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -1,15 +1,5 @@
 <?php
-if(isset($_SESSION['user_id'])){
-   $e = $_SESSION['user_id'];
-   $p = $_SESSION['password'];
-   
-   echo $e;
-   echo $p;
-
-}
-else {
-
-    header("Location : http://35.238.89.60/Portal_RTDS/login.php");
-}
+$e =$_SESSION['user_id'];
+echo $e;
 ?>
 
