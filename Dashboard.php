0681d0cdadadb803c60f38917ec9984ace323e06
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-04 10:21:03 +0530

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -1,35 +1,3 @@
 <?php
-session_start();
-if(!isset($_SESSION['user_id'])){
-    header("Location : http://35.238.89.60/Portal_RTDS/login.php");
-}
-echo $_SESSION['user_id'];die;
+include 'sessioncheck.php';
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-
-<button type="button" onclick="sessiondes()">Logout</button>
-
-
-<script>
-    function sessiondes() {
-        <?php
-            session_unset();
-            session_destroy();
-        ?>
-}
-
-
-</body>
-</script>
-</html>
