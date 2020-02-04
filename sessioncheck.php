6ceaf12166741d576718247c4775109fb84eca02
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-04 11:15:08 +0530

    Update sessioncheck.php

--- sessioncheck.php
+++ sessioncheck.php
@@ -1,15 +1,6 @@
 <?php
-$flag = $_POST['f'];
-
+session_start();
 if(!isset($_SESSION['user_id'])){
     header("Location : http://35.238.89.60/Portal_RTDS/login.php");
-}
-else if($flag == 1){
-    session_destroy();
-    header("Location : http://35.238.89.60/Portal_RTDS/login.php");
-}
-
-else {
-    session_start();
 }
 ?>
