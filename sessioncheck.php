9efba271af5628fbf9f4719f965cfdcecf41978e
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-04 12:30:49 +0530

    Update sessioncheck.php

--- sessioncheck.php
+++ sessioncheck.php
@@ -3,6 +3,6 @@ session_start();
 if(empty($_SESSION['user_id'])){
 
     header("Location: http://35.238.89.60/Portal_RTDS/login.php");
-    echo $_SESSION['user_id']; die;
+    echo $_SESSION['user_id'];
 }
 ?>
