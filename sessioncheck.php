e4513244c9a4c00985250e998e27eb21f794d6c9
Archit-erp <38176948+Archit-erp@users.noreply.github.com>
2020-02-04 10:30:07 +0530

    Update sessioncheck.php

--- sessioncheck.php
+++ sessioncheck.php
@@ -5,31 +5,3 @@ if(!isset($_SESSION['user_id'])){
 }
 echo $_SESSION['user_id'];
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-
-<button type="button" onclick="sessiondes()">Logout</button>
-
-
-<script>
-    function sessiondes() {
-        <?php
-        //session_unset();
-        session_destroy();
-        ?>
-    }
-
-</script>
-    </body>
-
-</html>
